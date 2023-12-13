262f5915791b0debe6914d29b88bffa860a1062f
Ragip Topalli <ragip.topalli@pabau.com>
2023-12-13 18:36:40 +0100

    feat: added clerk to deal with authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex flex-col gap-y-4'>
-      <nav className='w-full bg-slate-400'>Auth Navbar</nav>
-      {children}
-    </div>
+    <div className='h-full flex items-center justify-center'>{children}</div>
   );
 };
 
