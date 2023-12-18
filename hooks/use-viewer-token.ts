3fd27afe0fc967ba1185eca2d207313b06c1f398
Ragip Topalli <ragip.topalli@pabau.com>
2023-12-18 22:35:15 +0100

    feat: started implementing the stream player component, created a server action to create a viewer token then created a hook to use that to check for the user

---
+++ hooks/use-viewer-token.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+import { type JwtPayload, jwtDecode } from 'jwt-decode';
+
+import { toast } from 'sonner';
+import { createViewerToken } from '@/actions/token';
+
+export const useViewerToken = (hostIdentity: string) => {
+  const [token, setToken] = useState('');
+  const [name, setName] = useState('');
+  const [identity, setIdentity] = useState('');
+
+  useEffect(() => {
+    const createToken = async () => {
+      try {
+        const viewerToken = await createViewerToken(hostIdentity);
+        setToken(viewerToken);
+
+        const decodedToken = jwtDecode(viewerToken) as JwtPayload & {
+          name?: string;
+        };
+        const name = decodedToken?.name;
+        const identity = decodedToken.jti;
+
+        if (identity) {
+          setIdentity(identity);
+        }
+
+        if (name) {
+          setName(name);
+        }
+      } catch (error) {
+        toast.error(`Something went wrong: ${error}`);
+      }
+    };
+
+    createToken();
+  }, [hostIdentity]);
+
+  return {
+    token,
+    name,
+    identity,
+  };
+};
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -5,6 +5,20 @@ export const getUserByUsername = async (username: string) => {
     where: {
       username,
     },
+    include: {
+      Stream: true,
+    },
+  });
+
+  return user;
+};
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      Stream: true,
+    },
   });
 
   return user;
